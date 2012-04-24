427722d3356e13094cea3d8aed9ab7b7b63a6292
Tate Johnson <tate@tatey.com>
2012-04-24 21:41:25 +1000

    Initial Commit

---
+++ NextStop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NextStop
+//
+//  Created by Tate Johnson on 24/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NextStopTests/NextStopTests.h
@@ -0,0 +1,13 @@
+//
+//  NextStopTests.h
+//  NextStopTests
+//
+//  Created by Tate Johnson on 24/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NextStopTests : SenTestCase
+
+@end
