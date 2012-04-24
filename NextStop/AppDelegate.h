4aa45f0951583d95757afdd3d53dc6dec66c6e85
Tate Johnson <tate@tatey.com>
2012-04-24 21:47:16 +1000

    Formatting.

--- NextStop/AppDelegate.h
+++ NextStop/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  NextStop
-//
-//  Created by Tate Johnson on 24/04/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
