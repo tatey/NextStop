9fa66e2848d5e4e67270540ea9c1ea1ba73c98d1
Tate Johnson <tate@tatey.com>
2012-04-25 23:46:50 +1000

    AppDelegate instantiates RoutesViewController as its root view controller.

--- NextStop/AppDelegate.m
+++ NextStop/AppDelegate.m
@@ -1,19 +1,23 @@
 #import "AppDelegate.h"
 #import "MapViewController.h"
-#import "User.h"
+#import "RoutesViewController.h"
 
 @implementation AppDelegate
 
 @synthesize window = _window;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(37.786996, -122.419281);
-    User *user = [[User alloc] initWithCoordinate:coordinate routeCode:@"133"];
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = [[MapViewController alloc] initWithUser:user];
+    self.window.rootViewController = [self rootViewController];
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+- (UIViewController *)rootViewController {
+    UIViewController *rootViewController = [[RoutesViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:rootViewController];
+    return navigationController;
 }
 
 @end
