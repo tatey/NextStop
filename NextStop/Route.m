44a04e5f55568c69d42d010c4f767b15d7f351ce
Tate Johnson <tate@tatey.com>
2012-04-25 22:48:13 +1000

    Do not copy the string. The pointer will still be around before the query is performed.

--- NextStop/Route.m
+++ NextStop/Route.m
@@ -7,8 +7,8 @@
     SQLiteDB *db = [SQLiteDB sharedDB];
     sqlite3_stmt *stmt = [db prepareStatementWithQuery:@"SELECT * FROM routes WHERE code LIKE ? OR name LIKE ?"];
     const char *wildcard = [[NSString stringWithFormat:@"%%%@%%", codeOrName] UTF8String];
-    sqlite3_bind_text(stmt, 1, wildcard, -1, SQLITE_TRANSIENT);
-    sqlite3_bind_text(stmt, 2, wildcard, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 1, wildcard, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, wildcard, -1, SQLITE_STATIC);
     NSMutableArray *routes = [NSMutableArray array];
     [db performAndFinalizeStatement:stmt blockForEachRow:^(sqlite3_stmt *stmt) {
         Route *route = [[self alloc] initWithStatement:stmt];
