48e1f0f8dfa32dd5f0b95a56162ce012e0b21a28
Tate Johnson <tate@tatey.com>
2012-04-25 12:09:46 +1000

    AppDelegate initializes root view controller with user.

--- NextStop/AppDelegate.m
+++ NextStop/AppDelegate.m
@@ -1,14 +1,17 @@
 #import "AppDelegate.h"
 #import "MapViewController.h"
+#import "User.h"
 
 @implementation AppDelegate
 
 @synthesize window = _window;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(37.786996, -122.419281);
+    User *user = [[User alloc] initWithCoordinate:coordinate routeCode:@"133"];
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = [[MapViewController alloc] init];
+    self.window.rootViewController = [[MapViewController alloc] initWithUser:user];
     [self.window makeKeyAndVisible];
     return YES;
 }
