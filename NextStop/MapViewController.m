92cd7c02dd9ecc948fb3f33b5176d7b5eb6ee377
Tate Johnson <tate@tatey.com>
2012-04-27 22:14:23 +1000

    KVOSEL category on NSObject implements observeValueForKeyPath:ofObject:change:context with selector as context.

--- NextStop/MapViewController.m
+++ NextStop/MapViewController.m
@@ -1,5 +1,6 @@
 #import "Annotation.h"
 #import "MapViewController.h"
+#import "NSObject+KVOSEL.h"
 #import "User.h"
 
 #define DURATION 0.75
@@ -49,17 +50,6 @@ static NSString *const kUserCoordinateKeyPath = @"user.coordinate";
 - (void)viewWillDisappear:(BOOL)animated {
     [self removeObserver:self forKeyPath:kUserCoordinateKeyPath];
     [super viewWillDisappear:animated];
-}
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-    #if defined (__clang__)
-        #pragma clang diagnostic push
-        #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-    #endif
-    [self performSelector:(SEL)context withObject:object withObject:change];
-    #if defined (__clang__)
-        #pragma clang diagnostic pop
-    #endif
 }
 
 - (void)user:(id)object coordinateDidChange:(NSDictionary *)change {
---
+++ NextStop/NSObject+KVOSEL.m
@@ -0,0 +1,16 @@
+#import "NSObject+KVOSEL.h"
+
+@implementation NSObject (KVOSEL)
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    #if defined (__clang__)
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    #endif
+    [self performSelector:(SEL)context withObject:object withObject:change];
+    #if defined (__clang__)
+    #pragma clang diagnostic pop
+    #endif
+}
+
+@end
