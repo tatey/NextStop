7736636aeed718235ba15b0ec9322dd08ab8eb86
Tate Johnson <tate@tatey.com>
2012-08-29 00:01:49 +1000

    Add monitored property to StopAnnotationView.

--- NextStop/StopAnnotationView.h
+++ NextStop/StopAnnotationView.h
@@ -3,6 +3,11 @@
 
 @interface StopAnnotationView : MKPinAnnotationView
 
+@property (assign, nonatomic, getter = isMonitored) BOOL monitored;
 @property (assign, nonatomic, getter = isTargeted) BOOL targeted;
+
+- (void)setMonitored:(BOOL)monitored animated:(BOOL)animated;
+
+@end
 
 @end
--- NextStop/StopAnnotationView.m
+++ NextStop/StopAnnotationView.m
@@ -1,25 +1,62 @@
 #import "StopAnnotationView.h"
 
+@interface StopAnnotationView ()
+
+@property (strong, nonatomic) UISwitch *monitorSwitch;
+
+@end
+
 @implementation StopAnnotationView
 
+// Public
+@synthesize monitored = _monitored;
 @synthesize targeted = _targeted;
 
-- (id)initWithAnnotation:(id<MKAnnotation>)annotation reuseIdentifier:(NSString *)reuseIdentifier {
+// Private
+@synthesize monitorSwitch = _monitorSwitch;
+
+- (id)initWithAnnotation:(id <MKAnnotation>)annotation reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithAnnotation:annotation reuseIdentifier:reuseIdentifier];
     if (self) {
         self.canShowCallout = YES;
+        self.monitored = NO;
         self.targeted = NO;
+        self.monitorSwitch = [[UISwitch alloc] init];
+        [self.monitorSwitch addTarget:self action:@selector(monitorSwitchDidChangeValue:) forControlEvents:UIControlEventValueChanged];
+        self.leftCalloutAccessoryView = self.monitorSwitch;
     }
     return self;
 }
 
+- (void)setMonitored:(BOOL)monitored {
+    [self setMonitored:monitored animated:NO];
+}
+
+- (void)setMonitored:(BOOL)monitored animated:(BOOL)animated {
+    _monitored = monitored;
+    [self.monitorSwitch setOn:monitored animated:animated];
+    [self changePinColor];
+}
+
 - (void)setTargeted:(BOOL)targeted {
     _targeted = targeted;
-    if (_targeted) {
+    [self changePinColor];
+}
+
+- (void)changePinColor {
+    if (self.monitored && self.targeted) {
         self.pinColor = MKPinAnnotationColorGreen;
-    } else {
+    } else if (self.targeted) {
         self.pinColor = MKPinAnnotationColorRed;
+    } else {
+        self.pinColor = MKPinAnnotationColorPurple;
     }
+}
+
+#pragma mark - Actions
+
+- (void)monitorSwitchDidChangeValue:(UISwitch *)monitorSwitch {
+    self.monitored = monitorSwitch.on;
 }
 
 @end
