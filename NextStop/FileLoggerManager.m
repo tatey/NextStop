5b4e87b34bf6e006d5f8cc5d2ec44010e341cf9e
Tate Johnson <tate@tatey.com>
2012-11-11 12:26:15 +1100

    Change value to 10kb per file, 20kb in total. This ought to be enough.

--- NextStop/FileLoggerManager.m
+++ NextStop/FileLoggerManager.m
@@ -16,6 +16,9 @@
     self = [super init];
     if (self) {
         self.fileLogger = [[DDFileLogger alloc] init];
+        self.fileLogger.rollingFrequency = -1; // Do not roll based on time
+        self.fileLogger.maximumFileSize = (1024 * 10); // 10kb
+        self.fileLogger.logFileManager.maximumNumberOfLogFiles = 2;
     }
     return self;
 }
